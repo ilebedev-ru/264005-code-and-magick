5c53f620298df81fbaac7c18c788b683b389080f
ilebedev-ru <wapstyle@bk.ru>
2018-09-07 22:25:43 +0500

    правки: enum константы, поиск максимального, hsla цвет, поиск случайного в отдельную функцию

--- js/stat.js
+++ js/stat.js
@@ -1,26 +1,36 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var CLOUD_GAP = 10;
+var Cloud = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10,
+  GAP: 10,
+  COLOR: '#fff',
+  SHADOW: 'rgba(0, 0, 0, 0.7)'
+};
 
-var FONT_GAP = 15;
+var Font = {
+  STYLE: '16px PT Mono',
+  COLOR: '#000000',
+  GAP: 15
+};
 
-var BARCHART_HEIGHT = 150;
-var BAR_WIDTH = 40;
-var BAR_GAP = 50;
-var BARCHART_Y = 100;
+var BarChart = {
+  Y: 100,
+  HEIGHT: 150,
+  BAR_WIDTH: 40,
+  GAP: 50
+};
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, Cloud.WIDTH, Cloud.HEIGHT);
 };
 
 var findMaxValue = function (array) {
   var max = array[0];
-  for (var i = 0; i < array.length; i++) {
+  for (var i = 1; i < array.length; i++) {
     if (array[i] > max) {
       max = array[i];
     }
@@ -28,35 +38,38 @@ var findMaxValue = function (array) {
   return max;
 };
 
+var getRandomNumber = function (min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  rand = Math.round(rand);
+  return rand;
+};
+
 window.renderStatistics = function (ctx, names, times) {
 
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, Cloud.X + Cloud.GAP, Cloud.Y + Cloud.GAP, Cloud.SHADOW);
+  renderCloud(ctx, Cloud.X, Cloud.Y, Cloud.COLOR);
 
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_GAP, 40);
-  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_GAP, 60);
+  ctx.fillStyle = Font.COLOR;
+  ctx.font = Font.STYLE;
+  ctx.fillText('Ура вы победили!', Cloud.X + Font.GAP, Cloud.Y + Font.GAP * 2);
+  ctx.fillText('Список результатов:', Cloud.X + Font.GAP, Cloud.Y + Font.GAP * 3);
 
   var maxValue = findMaxValue(times);
 
   for (var i = 0; i < names.length; i++) {
-    var barHeight = BARCHART_HEIGHT * times[i] / maxValue;
-    var barXPoint = CLOUD_X + BAR_WIDTH * i + BAR_GAP * (i + 1);
-    var barYPoint = BARCHART_Y + BARCHART_HEIGHT - barHeight;
+    var barHeight = BarChart.HEIGHT * times[i] / maxValue;
+    var barXPoint = Cloud.X + BarChart.BAR_WIDTH * i + BarChart.GAP * (i + 1);
+    var barYPoint = BarChart.Y + BarChart.HEIGHT - barHeight;
 
-    ctx.fillStyle = 'rgba(8, 8, 255, 1)';
-    ctx.filter = 'saturate(' + Math.floor((Math.random() * 100) + 1) + '%)';
+    ctx.fillStyle = 'hsla(241, ' + getRandomNumber(0, 100) + '%, 30%, 1)';
 
     if (names[i] === 'Вы') {
-      ctx.filter = 'none';
       ctx.fillStyle = 'red';
     }
 
-    ctx.fillRect(barXPoint, barYPoint, BAR_WIDTH, barHeight);
-    ctx.filter = 'none';
-    ctx.fillStyle = '#000000';
-    ctx.fillText(Math.round(times[i]), barXPoint, barYPoint - FONT_GAP);
-    ctx.fillText(names[i], barXPoint, BARCHART_Y + BARCHART_HEIGHT + FONT_GAP);
+    ctx.fillRect(barXPoint, barYPoint, BarChart.BAR_WIDTH, barHeight);
+    ctx.fillStyle = Font.COLOR;
+    ctx.fillText(Math.round(times[i]), barXPoint, barYPoint - Font.GAP);
+    ctx.fillText(names[i], barXPoint, BarChart.Y + BarChart.HEIGHT + Font.GAP);
   }
 };
